0fbaaeb078bed6fce760ef1a155ff6c971487eae
Guillaume Roussin <guillaume.roussin44@gmail.com>
2023-10-31 08:29:27 +0100

    Created graph data based on map

--- src/app.py
+++ src/app.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from PIL import Image, ImageTk
-from os import path
 
 class App:
 
@@ -17,7 +16,6 @@ class App:
         self.canvas.pack()
 
         self.set_map_as_bg(path_to_map)
-        self.root.bind("<Button-1>", self.on_mouse_pressed)
 
 
     def set_map_as_bg(self, path_to_map: str):
@@ -30,22 +28,14 @@ class App:
         self.canvas.create_image(0, 0, anchor=tk.NW, image=tk_image)
         self.canvas.image = tk_image
 
-    def on_mouse_pressed(self, event: tk.Event):
-        x, y = event.x, event.y
-        self.canvas.create_oval(x - 5, y - 5, x + 5, y + 5, fill="red")
+    def on_click(self, callback):
+        self.root.bind("<Button-1>", lambda e: callback(e, self.canvas))
+
 
     def run(self):
         self.root.mainloop()
 
+
 # ============================================================
 
-def main():
-    assets_dir = path.abspath(path.join(path.dirname(__file__), "../assets"))
-    map_path = path.join(assets_dir, "map.png")
 
-    app = App("Map", 500, map_path)
-    app.run()
-
-
-if __name__ == "__main__":
-    main()
